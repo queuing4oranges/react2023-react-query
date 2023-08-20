7bff59b6093cb78d87b357817de04b1697953904
queuing4oranges <katjazenker@gmail.com>
2023-08-20 19:39:21 +0200

    Implementing useQueryClient

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import customFetch from './utils';
+import { toast } from 'react-toastify';
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
+  const queryClient = useQueryClient();
+
+  //destr - mutate comes from the axios obj.
+  const { mutate:createTask, isLoading } = useMutation({
+    mutationFn: (taskTitle) => customFetch.post('/', { title: taskTitle }), 
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey:['tasks']})  //invalidate = refetch data from the specified query or data source - here from the queryKey
+      toast.success('task added')
+      setNewItemName("")
+    }, 
+    onError: (error)=>{
+      toast.error(error.response.data.msg)
+    }
+  })
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    createTask(newItemName);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -16,7 +34,8 @@ const Form = () => {
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type='submit' className='btn'>
+        {/* disable button while it is submitting the task (so while loading) */}
+        <button type='submit' className='btn' disabled={ isLoading }> 
           add task
         </button>
       </div>
--- src/Items.jsx
+++ src/Items.jsx
@@ -2,21 +2,32 @@ import SingleItem from './SingleItem';
 import { useQuery } from '@tanstack/react-query';
 import customFetch from './utils';
 
-const Items = ({ items }) => {
+const Items = () => {
 
-  const { isLoading, data } = useQuery({
+  const { isLoading, data, error, isError } = useQuery({
     queryKey: ['tasks'], 
-    queryFn: () => customFetch.get('/')
+    // queryFn: () => customFetch.get('/') --> to avoid data.data.taskList, use async
+    queryFn: async() => {
+      const { data } = await customFetch.get('/')
+      return data;
+    }
   });
   
   if(isLoading) {
     return <p>Loading...</p>
   }
 
+  // if(isError) {
+  //   return <p>There was an error...</p> 
+  // }
+
+    if(error) {
+    return <p>{error.response.data}</p> //prints error message from axios
+  }
 
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
